3f488b6f3f24599fe8c251c3df56651dff63fcbb
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-01 22:50:00 -0300

    aula api 2

---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+//importando o o componente para ser estilizando
+import { NavDiv } from '../css/nav';
+
+function Nav() {
+  return (
+    // abertura do componente de estilização
+    <NavDiv>
+      <header className="header">
+        <nav className="headerMenu">
+          <ul>
+            <Link to="/" className="tlink">
+              Home
+            </Link>{' '}
+            {''}
+            <Link to="/escola" className="tlink">
+              Escola
+            </Link>
+            <Link to="/listarAlunos" className="tlink">
+              Listar Alunos
+            </Link>
+          </ul>
+        </nav>
+      </header>
+    </NavDiv>
+    //fechamento do componente de estilização
+  );
+}
+export default Nav;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './routers/Home.jsx';
+import Error from './routers/Error.jsx';
+import Escola from './routers/Escola.jsx';
+//importando o estilo geral com styled Components
+import StyloGeral from './css/estiloGlobal.jsx';
+import ListarAlunos from './routers/ListarAlunos.jsx'
+import Alunos from './routers/Alunos.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/escola', element: <Escola /> },
+      { path: '/listaralunos', element: <ListarAlunos /> },
+      { path: '/incluir', element: <Alunos /> },
+      { path: '/editar/:id', element: <Alunos /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <RouterProvider router={router} />
+    {/*chamando o componente de estilização (styled Components)*/}
+    <StyloGeral />
+  </>,
+);
